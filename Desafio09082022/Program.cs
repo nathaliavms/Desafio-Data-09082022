17b187b92b7f350fef6ac6b28478987ee834e53c
Nathalia Vieira <101611468+nathaliavms@users.noreply.github.com>
2022-08-10 11:36:56 -0300

    Update Program.cs

--- Desafio09082022/Program.cs
+++ Desafio09082022/Program.cs
@@ -1,59 +1,130 @@
 using System;
 
-namespace Desafio09082022
+
+namespace Exercicio
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Menu()
         {
-            Console.WriteLine("Digite o código de DDD para discagem interurbana:");
-            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escolha uma opção:");
+            Console.WriteLine("1 - Calculadora IMC");
+            Console.WriteLine("2 - Calculadora Area Circulo");
+            Console.WriteLine("3 - Calculadora regra de 3");
+            Console.WriteLine("4 - Sair");
 
-            Console.WriteLine(DddInformado(a));  
-            
+            return int.Parse(Console.ReadLine());
+
         }
 
-        static string DddInformado(int a)
+        static string RetornaClassificacaoImc(double a, double b)
         {
+            double calculo;
+            
 
-            if (a == 61)
+            calculo = a / (b * b);
+
+            if (calculo < 18.50)
             {
-                return "Brasilia";
+                return "Você está abaixo do peso";
             }
-            else if (a == 71)
+            else if (calculo < 25.00)
             {
-                return "Salvador";
+                return "Você está com o peso normal";
             }
-            else if (a == 11)
+            else if (calculo < 30)
             {
-                return "Sao Paulo";
+                return "Você está com sobrepeso";
             }
-            else if (a == 21)
+            else if (calculo < 35)
             {
-                return "Rio de Janeiro";
+                return "Você está com obesidade grau I";
             }
-            else if (a == 32)
+            else if (calculo < 40)
             {
-                return "Juiz de Fora";
+                return "Você está com obesidade grau II";
             }
-            else if (a == 19)
+            else if (calculo >= 40)
             {
-                return "Campinas";
-            }
-            else if (a == 27)
-            {
-                return "Vitoria";
-            }
-            else if (a == 31)
-            {
-                return "Belo Horizonte";
+                return "Você está com obesidade grau III";
             }
             else
             {
-                return "DDD nao cadastrado";
+                return "Invalido";
+            }
+        }
+
+        static void CalcularImc()
+        {
+            Console.WriteLine("Digite seu peso:");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite sua altura:");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine(RetornaClassificacaoImc(a, b));
+        }
+
+        static void CalcularArea()
+        {
+            
+            Console.WriteLine("Informe o raio do circulo");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(RetornarArea(a));
+
+        }
+
+        static double RetornarArea(double a)
+        {
+            double resultado;
+            resultado = 3.14 * (a * a);
+            return resultado;
+        }
+
+        static void CalcularRegra3()
+        {
+            
+            Console.WriteLine("Informe valores A, B e C (na mesma linha)");
+            string[] vet2 = Console.ReadLine().Split(' ');
+            double a = double.Parse(vet2[0]);
+            double b = double.Parse(vet2[1]);
+            double c = double.Parse(vet2[2]);
+            Console.WriteLine(RetornarRegra3(a,b,c));
+
+        }
+
+        static double RetornarRegra3(double a, double b, double c)
+        {
+            double resultado;
+            resultado = (a * c) / b;
+
+            return resultado;
+        }
+
+        static void Main(string[] args)
+        {
+            var a = Menu();
+
+            if (a == 1)
+            {
+                CalcularImc();
+ 
             }
 
-          
+            else if (a == 2)
+            {
+                CalcularArea();
+            }
+
+            else if (a == 3)
+            {
+                CalcularRegra3();
+
+            }
+            else 
+            {
+                Console.WriteLine("FINALIZADO COM SUCESSO!");
+            }
         }
+
     }
+
 }
